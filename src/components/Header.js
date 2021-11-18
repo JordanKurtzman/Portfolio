b6dbffafa994bdf30d9dd7e3fd25cab3505e1a16
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-11-17 17:04:17 -0800

    fixed 'active' class on responsive nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 
-
-
-
-
 export default class Header extends React.Component {
     constructor(props) {
         super(props)
@@ -19,12 +15,23 @@ export default class Header extends React.Component {
                isActive: !this.state.isActive}
         })
     }
+    componentDidMount(){
+        window.addEventListener('resize', () =>{
+            this.setState(() => {
+                if(window.innerWidth > 768){
+                    return {
+                        isActive: false
+                    }
+                }
+            })
+        })
+    }
 
     render(){
         return(
             <div>
-                <nav className={this.state.isActive? 'active' : null}>
-                    <ul className="nav__menu">
+                <nav>
+                    <ul className={this.state.isActive ? 'active' : null}>
                         <li>
                             <NavLink className="navlink" exact to="/" activeClassName="is-active">Home</NavLink>
                         </li>
@@ -34,10 +41,12 @@ export default class Header extends React.Component {
                         <li>
                             <NavLink className="navlink" to="/contact" activeClassName="is-active">Contact</NavLink>
                         </li>
-                        <button className="navtoggle" onClick={this.handleToggle}>
-                        </button>
+                        
 
                     </ul>
+                    <button className="navtoggle" onClick={this.handleToggle}>
+                        <i className=" navtoggle__icon fas fa-bars"></i>
+                    </button>
 
                 </nav>
             </div>
