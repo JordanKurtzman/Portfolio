484d8fe3f52dfebfbc75efec18a45486e5b0d2e7
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-11-28 16:11:37 -0800

    finished projects section

--- src/components/Images.js
+++ src/components/Images.js
@@ -7,5 +7,7 @@ import WebpackLogo from '../images/WebpackLogo.png'
 import ReactLogo from '../images/ReactLogo.png'
 import PhpLogo from '../images/PhpLogo.png'
 import HtmlLogo from '../images/HTML5Logo.png'
+import BeerApp from '../images/PunkAPIScreenShot.png'
+import ArtsCenter from '../images/WordPressScreenShot.png'
 
-export { BabelLogo, Bootstrap, JavaScriptLogo, SassLogo, CSSLogo, WebpackLogo, ReactLogo, PhpLogo, HtmlLogo }
+export { BabelLogo, Bootstrap, JavaScriptLogo, SassLogo, CSSLogo, WebpackLogo, ReactLogo, PhpLogo, HtmlLogo, BeerApp, ArtsCenter }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { BeerApp, ArtsCenter } from './Images'
 
 const Portfolio = () => {
     return(
-        <div>
-            <p>Here are a few examples of my work:</p>
-            <Link>Chloe Medrano Photography</Link>
-            <Link>Punk API App</Link>
-            <Link>Sample Wordpress Site: Community Art Center</Link>
+        <div className="portfoliocontainer">
+            
+            <div className="portfolio">
+                <div className="portfolio__piece">
+                    
+                    
+                    <div className="portfolio__piece--imgcontainer">
+                        <h3>Punk API App</h3>
+                        <img className="portfolio__piece--img" src={BeerApp} />
+                        <div className="fadebox">
+                            <p className="fadebox__p">A vanilla JS app that uses the Punk API, an opensource API that allows developers to search through Brewdog's catalog of beers, to search for specific types of beer and generate lists of beer based on certain criteria.</p>
+                            <p className="fadebox__plink">Github <a href="https://github.com/JordanKurtzman/Punk-API-App">repository</a></p>
+                            <p className="fadebox__plink">Check it out<a href="https://beer-app-dev-version.netlify.app/">here</a></p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="portfolio__piece">
+                    
+                    <div className="portfolio__piece--imgcontainer">
+                        <h3>Sample Wordpress Site: Community Art Center</h3>
+                        <img className="portfolio__piece--img" src={ArtsCenter} />
+                        <div className="fadebox">
+                            <p className="fadebox__p">A sample custom WordPress theme based on a WordPress site I built for a client.</p>
+                            <p className="fadebox__plink">Github <a href="https://github.com/JordanKurtzman/Arts-Center-WP">repository</a></p>
+                            <p className="fadebox__plink">Check it out<a href="http://jkurtzman-wpdemo.com/">here</a></p>
+                        </div>
+                    </div>
+                    
+
+                </div>
+
+
+            </div>
         </div>
+        
     )
     
 }
