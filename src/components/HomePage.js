21825b1099edddf97207da8477615d830f9ee912
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-12-17 21:05:29 -0800

    added anchor tag to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HashLink as Link } from 'react-router-hash-link'
 import { BabelLogo, Bootstrap, JavaScriptLogo, SassLogo, CSSLogo, WebpackLogo, ReactLogo, PhpLogo, HtmlLogo, Headshot } from './Images'
 
 const HomePage = () => {
@@ -8,7 +9,7 @@ const HomePage = () => {
                 <div className="homepage__text">
                     <h1>Hi! I'm Jordan. 👋</h1>
                     <p>I'm a front-end developer based in Los Angeles. I have a background in the arts, and I love that front-end development lets me use both sides of my brain. If you'd like to check out my github profile, you can do so <a className="homepage__link" href="https://github.com/JordanKurtzman">here</a>
-                        . Here are some programming languages and technologies that I have experience working with:</p>
+                        . Here are some programming languages and technologies that I have experience working with<Link smooth className="hashlink" to="#sectiontwo"><i class="fas fa-caret-down"></i></Link></p>
                 </div>
                 <div className='homepage__picture'>
                     <img className="homepage__picture--img" src={Headshot} />
@@ -16,7 +17,7 @@ const HomePage = () => {
         </div>
             
             
-           
+            <div className="logo__section" id="sectiontwo">
                 <div className="logo__grid">
                     <div className="logo__container"><img className="logo" src={Bootstrap} alt="Bootstrap" /></div>
                     <div className="logo__container"><img className="logo" src={BabelLogo} alt="BabelLogo" /></div>
@@ -28,6 +29,8 @@ const HomePage = () => {
                     <div className="logo__container"><img className="logo" src={HtmlLogo} alt="Html5Logo" /></div>
                     <div className="logo__container"><img className="logo" src={PhpLogo} alt="PhpLogo" /></div>
                 </div>
+           </div>
+                
             
             
         </div>
