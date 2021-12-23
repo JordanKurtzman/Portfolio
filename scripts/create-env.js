395f7190b29cf694b21860308d0c75a93ac76103
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-12-22 22:41:03 -0800

    another change to create env

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,8 @@
 const fs = require('fs')
-fs.writeFileSync('./.env.')
+const envData = [
+    `MAILTRAP_PASSWORD=${MAILTRAP_PASSWORD.process.env}`,
+    `MAILTRAP_USERNAME=${MAILTRAP_USERNAME.process.env}`,
+    `RECEIVER_EMAIL=${RECEIVER_EMAIL.process.env}`
+]
+
+fs.writeFileSync('./.env.', envData.join('\n'))
