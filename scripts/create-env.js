5026cb9f5a23d5b662ac566f6a1d0d1a3bca4307
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-12-23 22:02:37 -0800

    converted to netlify form

--- scripts/create-env.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const envData = [
-    `MAILTRAP_PASSWORD=${MAILTRAP_PASSWORD.process.env}`,
-    `MAILTRAP_USERNAME=${MAILTRAP_USERNAME.process.env}`,
-    `RECEIVER_EMAIL=${RECEIVER_EMAIL.process.env}`
-]
-
-fs.writeFileSync('./.env.', envData.join('\n'))
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import process from 'process'
+import { useNetlifyForm, NetlifyFormProvider, NetlifyFormComponent, Honeypot } from 'react-netlify-forms'
 
 const ContactForm = () => {
     const { register, handleSubmit, reset, formState: { isSubmitSuccessful, isSubmitted, errors } } = useForm({
@@ -12,47 +12,70 @@ const ContactForm = () => {
         },
         
     })
+    const netlify = useNetlifyForm({
+        name: 'Contact',
+        action: '/send',
+        honeypotName: 'bot-field',
+        onSuccess: (response, context) => {
+            console.log('Successfully sent form data to Netlify server')
+        }
+    })
+    const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
+
     
 
-    const onSubmit = (data) =>    
-        Email.send({
-            Host: 'smtp.mailtrap.io',
-            Username: process.env.MAILTRAP_USERNAME,
-            Password: process.env.MAILTRAP_PASSWORD,
-            To: process.env.RECEIVER_EMAIL,
-            From: data.email,
-            Subject: "New contact form submission",
-            Body: `<p>You have a message from ${data.name} <${data.email}></p>
-                            <p>${data.message}</p>`,
-        }).then(
-            message => console.log(message),
-            reset()
+    // const onSubmit = (data) =>    
+    //     Email.send({
+    //         Host: 'smtp.mailtrap.io',
+    //         Username: process.env.MAILTRAP_USERNAME,
+    //         Password: process.env.MAILTRAP_PASSWORD,
+    //         To: process.env.RECEIVER_EMAIL,
+    //         From: data.email,
+    //         Subject: "New contact form submission",
+    //         Body: `<p>You have a message from ${data.name} <${data.email}></p>
+    //                         <p>${data.message}</p>`,
+    //     }).then(
+    //         message => console.log(message),
+    //         reset()
             
 
-        ).catch((error) => {
-            console.log(error),
-            reset()
+    //     ).catch((error) => {
+    //         console.log(error),
+    //         reset()
             
 
-    })
+    // })
 
     return (
 
         <div className='contactcontainer'>
             <div className='contact'>
                 <h1 className="contact__heading">Get in touch</h1>
-                {isSubmitSuccessful && <p>Thank you for reaching out!</p>}
-                {(!isSubmitSuccessful && isSubmitted) && <p>There was an error in submitting your form - please try again.</p>}
-                    <form onSubmit={handleSubmit(onSubmit)} className='contact__form'>
-
-                        <input  {...register('name', {required: true})} id='name' className="contact__form--input"/>
-                        {errors.name?.type === 'required' && <p>Name is required</p>}
-                        <input {...register('email', { required: true, pattern: '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'})} id='email' className="contact__form--input"/>
-                        {errors.email?.type === 'required' && <p>Email is required</p>}   
-                        {errors.email?.type === 'pattern' && <p>Please enter a valid email</p>} 
-                        <textarea {...register('message')} id='message' className="contact__form--textarea"/>
-                    <button type="submit" className="contact__form--submit">Submit</button>
-                    </form>
+                
+                <NetlifyFormProvider {...netlify}>
+                    <NetlifyFormComponent className='contact__form' onSubmit={handleSubmit(onSubmit)}>
+                        <Honeypot/>
+                        {netlify.success && <p>Thank you for reaching out!</p>}
+                        {(!isSubmitSuccessful && isSubmitted) && <p>There was an error in submitting your form - please try again.</p>}
+                            <input  
+                            {...register('name', 
+                            { required: true })}
+                            id='name' className="contact__form--input" />
+                            {errors.name?.type === 'required' && <p>Name is required</p>}
+                            
+                            <input 
+                            
+                            {...register('email', 
+                            { required: true, 
+                                pattern: '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/' })}
+                                id='email'
+                                className="contact__form--input" />
+                            {errors.email?.type === 'required' && <p>Email is required</p>}
+                            {errors.email?.type === 'pattern' && <p>Please enter a valid email</p>}
+                            <textarea {...register('message')} id='message' className="contact__form--textarea" />
+                            <button type="submit" className="contact__form--submit">Submit</button>
+                    </NetlifyFormComponent>    
+                </NetlifyFormProvider>
             </div>
         </div>
     )
