ca55fbcc05f38f726fc5ef6a5b406d94ebb8e4ed
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-12-24 22:49:41 -0800

    new contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,90 +1,52 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { useNetlifyForm, NetlifyFormProvider, NetlifyFormComponent, Honeypot } from 'react-netlify-forms'
+import react, {useState, useEffect} from 'react'
 
-const ContactForm = () => {
-    const { register, handleSubmit, reset, formState: { isSubmitSuccessful, isSubmitted, errors } } = useForm({
-        mode: 'onBlur',
-        defaultValues: {
-            name: '',
-            email: '',
-            message: ''
-        },
-        
-    })
-    const netlify = useNetlifyForm({
-        name: 'react-hook-form',
-        action: '/contact',
-        honeypotName: 'bot-field',
-        onSuccess: (response, context) => {
-            console.log('Successfully sent form data to Netlify server')
+export default function Contact() {
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        if (window.location.search.includes('success=true')) {
+            setSuccess(true);
         }
-    })
-    const onSubmit = (data) => netlify.handleSubmit(null, data).then(() =>{
-        console.log('submitted'),
-        reset()
-    })
-
-
-    const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
-
-    
-
-    // const onSubmit = (data) =>    
-    //     Email.send({
-    //         Host: 'smtp.mailtrap.io',
-    //         Username: process.env.MAILTRAP_USERNAME,
-    //         Password: process.env.MAILTRAP_PASSWORD,
-    //         To: process.env.RECEIVER_EMAIL,
-    //         From: data.email,
-    //         Subject: "New contact form submission",
-    //         Body: `<p>You have a message from ${data.name} <${data.email}></p>
-    //                         <p>${data.message}</p>`,
-    //     }).then(
-    //         message => console.log(message),
-    //         reset()
-            
-
-    //     ).catch((error) => {
-    //         console.log(error),
-    //         reset()
-            
-
-    // })
+    }, []);
 
     return (
-
-        <div className='contactcontainer'>
+        <div className="contactcontainer">
             <div className='contact'>
                 <h1 className="contact__heading">Get in touch</h1>
+                {success && (
+                    <p>Thanks for your message! </p>
+                )}
                 
-                <NetlifyFormProvider {...netlify}>
-                    <NetlifyFormComponent className='contact__form' onSubmit={handleSubmit(onSubmit)}>
-                        <Honeypot/>
-                        {netlify.success && <p>Thank you for reaching out!</p>}
-                        {(!isSubmitSuccessful && isSubmitted) && <p>There was an error in submitting your form - please try again.</p>}
-                            <input  
-                            {...register('name', 
-                            { required: true })}
-                            id='name' className="contact__form--input" />
-                            {errors.name?.type === 'required' && <p>Name is required</p>}
-                            
-                            <input 
-                            
-                            {...register('email', 
-                            { required: true, 
-                                pattern: '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/' })}
-                                id='email'
-                                className="contact__form--input" />
-                            {errors.email?.type === 'required' && <p>Email is required</p>}
-                            {errors.email?.type === 'pattern' && <p>Please enter a valid email</p>}
-                            <textarea {...register('message')} id='message' className="contact__form--textarea" />
-                            <button type="submit" className="contact__form--submit">Submit</button>
-                    </NetlifyFormComponent>    
-                </NetlifyFormProvider>
+                    <form
+                        name="contact"
+                        method="POST"
+                        action="/contact/?success=true"
+                        data-netlify="true"
+                    >
+                        <input type="hidden" name="form-name" value="contact" />
+                        <input
+                        type="text"
+                        name='name'
+                        className="contact__form--input"
+                        placeholder='Your name'/>
+                    <input
+                        type="email"
+                        name='email'
+                        className="contact__form--input"
+                        placeholder='Your email' />
+                    <textarea
+                    name='message'
+                    className="contact__form--textarea" />    
+
+
+                        
+                        
+                        <div>
+                            <Button type="submit">Send</Button>
+                        </div>
+                    </form>
+                
             </div>
         </div>
     )
 }
-
-export default ContactForm
