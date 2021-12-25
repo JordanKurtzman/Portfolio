a3fd1c780bdeaf859d4c7d2145651a52d393b2e6
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-12-25 12:27:21 -0800

    trying out formik again

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,57 +1,112 @@
 import React, {useState, useEffect} from 'react'
+import { Form, Field, ErrorMessage, Formik } from 'formik';
 
-export default function Contact() {
-    const [success, setSuccess] = useState(false);
+export default Contact = (props) => {
+    <Formik
 
-    useEffect(() => {
-        if (window.location.search.includes('success=true')) {
-            setSuccess(true);
+        initialValues={{
+            name: '',
+            email: '',
+            message: ''
+        }}
+        onSubmit={
+            (values, actions) => {
+                fetch("/", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                    body: encode({ "form-name": "contact", ...values })
+                })
+                    .then(() => {
+                        alert('Success');
+                        actions.resetForm()
+                    })
+                    .catch(() => {
+                        alert('Error');
+                    })
+                    .finally(() => actions.setSubmitting(false))
+            }
+            
         }
-    }, []);
+        validate={values => {
+            const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+            const errors = {};
+            if (!values.name) {
+                errors.name = 'Name Required'
+            }
+            if (!values.email || !emailRegex.test(values.email)) {
+                errors.email = 'Valid Email Required'
+            }
+            if (!values.message) {
+                errors.message = 'Message Required'
+            }
+            return errors;
+        }}
 
-    const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
-
-    return (
+    
+    
+    />
+        {props => (
         <div className="contactcontainer">
             <div className='contact'>
                 <h1 className="contact__heading">Get in touch</h1>
                 {success && (
                     <p>Thanks for your message! </p>
                 )}
-                
-                    <form
+
+                <Form
                     className='contact__form'
-                        name="contact"
-                        method="POST"
-                        action="/contact/?success=true"
-                        data-netlify="true"
-                    >
-                        <input type="hidden" name="form-name" value="contact" />
-                        <input
+                    name="contact"
+                    action="/contact/?success=true"
+                    data-netlify="true"
+                    onSubmit={props.handleSumbit}
+                >
+                    <input type="hidden" name="form-name" value="contact" />
+
+                    <Field
                         className='contact__form--input'
                         type="text"
                         name='name'
                         placeholder='Your name'
-                        required/>
-                    <input
+                        value={props.values.name}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        />
+                    <ErrorMessage name="name" />
+
+                    <Field
                         type="email"
                         name='email'
                         className="contact__form--input"
-                        placeholder='Your email' 
-                        required/>
-                    <textarea
-                    name='message'
-                    className="contact__form--textarea" />    
+                        placeholder='Your email'
+                        value={props.values.email}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        />
+                    <ErrorMessage name="email" />
+                    <Field
+                        as='textarea'
+                        name='message'
+                        className="contact__form--textarea"
+                        value={props.values.message}
+                        onChange={props.handleChange} 
+                        onBlur={props.handleBlur}
+                        />
+                    <ErrorMessage name="message" />
 
 
-                        
-                        
-                        <div>
+
+
+                    <div>
                         <button className="contact__form--submit" type="submit">Send</button>
-                        </div>
-                    </form>
-                
+                    </div>
+                </Form>
+
             </div>
         </div>
-    )
+
+        )}
+
+    
+        
+    
 }
