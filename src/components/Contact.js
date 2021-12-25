a061614068a9cab9bb28044c981092e8d68e077b
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-12-24 22:54:25 -0800

    fixed stupid react import error

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import react, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Contact() {
     const [success, setSuccess] = useState(false);
